215308c98e186dce584c8b5ab29a730ebd0bed9d
hwanghye00 <hwangshinhye.work@gmail.com>
2022-06-08 22:28:28 +0900

    Update articles, add detail edit delete

--- articles/views.py
+++ articles/views.py
@@ -1,19 +1,20 @@
 from turtle import title
 from django.shortcuts import render, redirect
 import random
+
+import articles
 from .models import Article
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
-def dinner(request, name):
+def dinner(request):
     menus = [{'name': '족발', 'price': 30000}, {'name': '햄버거', 'price': 5000}, {'name': '치킨', 'price': 20000}, {'name': '초밥', 'price': 15000}]
     pick = random.choice(menus)
     articles = Article.objects.order_by('-pk') 
     context = {
         'pick': pick,
-        'name': name,
         'menus': menus,
         'articles': articles,
     }
@@ -28,5 +29,26 @@ def create_review(request):
     article = Article(title=title, content=content)
     article.save()
 
-    return redirect('/articles/dinner/poco/')
+    return redirect('articles:detail', article.pk)
  
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article,
+    }
+    return render(request, 'detail.html', context)
+
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+
+    if request.method == 'POST':
+        article.delete()
+    
+    return redirect('articles:dinner')
+
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article,
+    }
+    return render(request, 'edit.html', context)
